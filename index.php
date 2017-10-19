4e19538979533a97f859e0b497832e96b774b38d
Jermain Jennings <jermainejennings05@gmail.com>
2017-10-19 09:26:01 -0600

    persona edited, attributes added. relations added.

--- index.php
+++ index.php
@@ -10,26 +10,27 @@
 			<h1><strong>Persona:</strong></h1>
 		<p>Bob is a 27 year old college graduate who enjoys the Tech world. Bob also
 			enjoys reading online articles and blogs on his spare time. Bob has recently relocated to
-			Albuquerque,New Mexico from Dallas,Texas. Bob enjoys computers and new modern day technonlogy
+			Albuquerque,New Mexico from Dallas,Texas. Bob enjoys computers and new modern day technology
 		and considers himself extremely tech savvy. Bob however gets extremely irritated when technology moves
 		slow or wont work at all. For this reason Bob is looking to switch cell phone providers.
 			Bob works as a paralegal for a law firm and most of his coworkers have T-Mobile or Verizon.
 			Bob wants to find out more about coverage with both of the carriers specifically
 			in certain areas around town. Bob turns to Reddit to submit a post and ask locals their
-			experience with coverage.</p>
+			experience with coverage. The current technology Bob uses is an iphone7
+			he is looking to upgrade to and iPhone X when he decides on what service provider to go with.</p>
 		<h1><strong>Story</strong></h1>
 		<p>As new user on Reddit Bob wants to create a post asking locals about cell phone coverage in the area.
 		Bob works for one of the best law firms in the state so it is important that he have the best cell phone coverage
-		possible to stay in contact with clients and colleges when both in and out of the office.<p/>
+		possible to stay in contact with clients and colleges when both in and out of the office.</p>
 
 		<uL>
 			<li>1. Open Reddit on your browser and sign in.</li>
 			<li>2. after contacting the server your browser will display your main home page after sigining in</li>
-			<li>3. Search for the catagory you want to post to.</li>
-			<li>4. The server will send any info it thinks it relevent to the topic typed
+			<li>3. Search for the category you want to post to.</li>
+			<li>4. The server will send any info it thinks it relevant to the topic typed
 				the the search field. and display it through your browser</li>
 			<li>5. Click Submit a text post </li>
-			<li>6. The browser will dispaly a screen with fields for you to type in.</li>
+			<li>6. The browser will display a screen with fields for you to type in.</li>
 			<li>7. Fill out the "title" and "text" fields as you wish. </li>
 			<li>8. Check the "im not a robot" box at the bottom of the page and click submit.</li>
 			<li>9. Once the website gives you confirmation that your
@@ -56,11 +57,13 @@
 			<h3><strong>Comment</strong></h3>
 				<ul>
 					<li>commentId(primary Key)</li>
-					<li>commentCommentId(Foregin Key)</li>
-					<li>commentedPostId(Foregin Key)</li>
-					<li>comment(Foregin Key)</li>
+					<li>commentCommentId(Foreign Key)</li>
+					<li>commentedPostId(Foreign Key)</li>
+					<li>comment(Foreign Key)</li>
 					<li>commentDateTime</li>
 				</ul>
+		<h4>Relations</h4>
 		<p>One post can have many comments (1 - n)</p>
+		<p>One comment can have many comments (1 - n)</p>
 	</body>
 </html>
