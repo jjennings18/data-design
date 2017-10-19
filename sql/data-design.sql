b89b47df19bb31d937e8698473da8514b312b141
Jermain Jennings <jermainejennings05@gmail.com>
2017-10-19 10:43:22 -0600

    Attributes added to tables

--- sql/data-design.sql
+++ sql/data-design.sql
@@ -1,14 +1,27 @@
+DROP TABLE IF EXISTS profile;
+DROP TABLE IF EXISTS post;
+DROP TABLE IF EXISTS comment;
+
   CREATE TABLE profile (
-profileId BINARY(20)
-profileAtHandel VARCHAR(30)
-ProfileEmail VARCHAR(200)
-profilePhone VARCHAR(20)
+profileId BINARY(20) NOT NULL,
+porfilActivationToken CHAR(32),
+profileAtHandel VARCHAR(30) NOT NULL,
+ProfileEmail VARCHAR(200) NOT NULL,
+profileHash VARCHAR(128) NOT NULL,
+profilePhone VARCHAR(20) NOT NULL,
+profilesalt CHAR(64) NOT NULL,
 PRIMARY KEY(profileId)
+UNIQUE(profileEmail)
+UNIQUE(profileAtHandle)
 )
+
+
   CREATE TABLE post (
-postId BINARY(20)
-postTopic VARCHAR(1000)
-postDateTime DATETIME(6)
+postId BINARY(20) NOT NULL,
+postProfileId BINARY(20) NOT NULL,
+postTopic VARCHAR(200) NOT NULL,
+postContent VARCHAR(4000) NOT NULL,
+postDateTime DATETIME(6) NOT NULL,
 INDEX(postId)
 PRIMARY KEY (postId)
 )
