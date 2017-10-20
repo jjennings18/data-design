8a1fcb1b357216b62dbaa507654445391fe07c69
Jermain Jennings <jermainejennings05@gmail.com>
2017-10-20 11:16:24 -0600

    spacing changed between attributes.

--- sql/data-design.sql
+++ sql/data-design.sql
@@ -4,50 +4,50 @@ DROP TABLE IF EXISTS comment;
 
 CREATE TABLE profile (
   -- primary key
-  profileId BINARY(16) NOT NULL,
-  porfilActivationToken CHAR(32),
-  profileAtHandle VARCHAR(30) NOT NULL,
-  ProfileEmail VARCHAR(200) NOT NULL,
-  profileHash VARCHAR(128) NOT NULL,
-  profilePhone VARCHAR(20) NOT NULL,
-  profilesalt CHAR(64) NOT NULL,
-  -- unique index made to avoid duplicates of profileEmail and profileHandle
-  UNIQUE(profileEmail),
-  UNIQUE(profileAtHandle),
-  -- defined primary key
-  PRIMARY KEY(profileId)
+	profileId BINARY(16) NOT NULL,
+	porfilActivationToken CHAR(32),
+	profileAtHandle VARCHAR(30) NOT NULL,
+	ProfileEmail VARCHAR(200) NOT NULL,
+	profileHash VARCHAR(128) NOT NULL,
+	profilePhone VARCHAR(20) NOT NULL,
+	profilesalt CHAR(64) NOT NULL,
+	-- unique index made to avoid duplicates of profileEmail and profileHandle
+	UNIQUE(profileEmail),
+	UNIQUE(profileAtHandle),
+	-- defined primary key
+	PRIMARY KEY(profileId)
 );
 
 
 CREATE TABLE post (
-  -- primary key
-  postId BINARY(16) NOT NULL,
-  -- foreign key
-  postProfileId BINARY(20) NOT NULL,
-  postTopic VARCHAR(200) NOT NULL,
-  postContent VARCHAR(4000) NOT NULL,
-  postDateTime DATETIME(6) NOT NULL,
-  INDEX(postId),
-  -- defind foreign key and relation
-  FOREIGN KEY (postProfileId) REFERENCES profile(profileId),
-  -- defind primary key
-  PRIMARY KEY (postId)
+	-- primary key
+	postId BINARY(16) NOT NULL,
+	-- foreign key
+	postProfileId BINARY(20) NOT NULL,
+	postTopic VARCHAR(200) NOT NULL,
+	postContent VARCHAR(4000) NOT NULL,
+	postDateTime DATETIME(6) NOT NULL,
+	INDEX(postId),
+	-- defind foreign key and relation
+	FOREIGN KEY (postProfileId) REFERENCES profile(profileId),
+	-- defind primary key
+	PRIMARY KEY (postId)
 );
 
 
 Create TABLE comment (
-  -- primary key
-  commentId BINARY(16) NOT NULL,
-  -- foreign key
-  commentProfileId BINARY(16) NOT NULL,
-  commentpostId Binary(16) NOT NULL,
-  commentContent VARCHAR (1000) NOT NULL,
-  commentDateTime DATETIME(6) NOT NULL,
-  INDEX(commentPostId),
-  -- defined primary key
-  PRIMARY KEY(commentId),
-  -- defined foreign key and relation
-  FOREIGN KEY(commentPostId) REFERENCES post (postId)
-
+	-- primary key
+	commentId BINARY(16) NOT NULL,
+	-- foreign key
+	commentProfileId BINARY(16) NOT NULL,
+	commentpostId Binary(16) NOT NULL,
+	commentContent VARCHAR (1000) NOT NULL,
+	commentDateTime DATETIME(6) NOT NULL,
+	INDEX(commentPostId),
+	-- defined primary key
+	PRIMARY KEY(commentId),
+	-- defined foreign key and relation
+	FOREIGN KEY(commentPostId) REFERENCES post (postId),
+	FOREIGN KEY(commentProfileId) REFERENCES profile (profileId)
 );
 
