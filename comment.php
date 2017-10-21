8b6ae9179f06d6bfe3500fbbb28fa26883d13d46
Jermain Jennings <jermainejennings05@gmail.com>
2017-10-21 15:26:54 -0600

    constructor method started for profile element.

---
+++ comment.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jermainejennings
+ * Date: 10/21/17
+ * Time: 1:54 PM
+ */
---
+++ profile.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * Description: Making class for profile
+ *
+ * Author: jermainjennings
+ *
+ * Date: 10/21/17
+ */
+class profile {
+
+	/**
+	 * id used for profile; this is the primary key
+	 * @var $profileId
+	 */
+	private $profileId;
+
+	/**
+	 * profileActivationToken; activation for profile
+	 * @var $profileActivationToken
+	 */
+	private $profileActivationToken;
+
+	/**
+	 * Handle for profile
+	 * @var $profileHandle
+	 */
+	private $profileHandle;
+
+/**
+ * email for profile
+ * @var $profileEmail
+ */
+private $profileEmail;
+
+/**
+ * Hash for profile
+ * @var  $profileHash
+ */
+private $profileHash;
+
+/**
+ * phone number for profile
+ * @var $profilePhone
+ */
+private $profilePhone;
+
+/**
+ * salt for profile
+ * @var $porfileSalt
+ */
+private $profileSalt;
+
+	/** constructor for this profile
+	 *
+	 */
+	public function __construct($newprofileid,$newprofileActivationToken,
+										 $newprofileHandle, $newprofileEmail, $newprofileHash,
+										 $newprofilePhone, $newprofileSalt  ) {
+										TRY {
+											$this->setprofileId($newprofileid);
+											$this->setprofileActivationToken($newprofileActivationToken);
+											$this->setprofileHandle($newprofileHandle);
+											$this->setprofileEmail($newprofileEmail);
+											$this->setprofileHash($newprofileHash);
+											$this->setprofilePhone($newprofilePhone);
+											$this->setprofileSalt($newprofileSalt);
+	}
+	}
+}
+
