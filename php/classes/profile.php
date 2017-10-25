d2f8c83a7e730cd71da7884be1e8bbbd2c75527a
Jermain Jennings <jermainejennings05@gmail.com>
2017-10-25 15:46:06 -0600

    accessor and mutator for activationToken added.

--- php/classes/profile.php
+++ php/classes/profile.php
@@ -92,3 +92,36 @@ public function setProfileId($newProfileId): void {
 	$this->profileId = $uuid
 }
 
+/**
+ * accessor method for account activation token
+ *
+ * @return string value of the activation token
+ **/
+public function getProfileActivationToken() : ?string {
+	return ($this->profileActivationToken);
+}
+
+/**
+ * mutator method for account activation token
+ *
+ * @param string $newProfileActivationToken
+ * @throws \InvalidArgumentException  if the token is not a string or insecure
+ * @throws \RangeException if the token is not exactly 32 characters
+ * @throws \TypeError if the activation token is not a string
+ **/
+public function setProfileActivationToken(?string $newProfileActivationToken): void {
+	if($newProfileActivationToken === null) {
+		$this->profileActivationToken = null;
+		return;
+	}
+	$newProfileActivationToken = strtolower(trim($newProfileActivationToken));
+	if(ctype_xdigit($newProfileActivationToken) === false) {
+		throw(new\RangeException("user activation is not valid"));
+	}
+	//make sure user activation token is only 32 characters
+	if(strlen($newProfileActivationToken) !== 32) {
+		throw(new\RangeException("user activation token has to be 32"));
+	}
+	$this->profileActivationToken = $newProfileActivationToken;
+}
+
